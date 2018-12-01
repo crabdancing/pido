714077fc604eccccb4e47904cad3c9cc5bca22ff
Cassie Nicol <cassieenicol@gmail.com>
2018-12-01 17:02:06 -0600

    bug fixes for sync options

--- check.sh
+++ check.sh
@@ -5,15 +5,33 @@ export PIDO='./pido'
 export TRANSACTION_OPTIONS='-D'
 #$PIDO --database --dbpath here -b there -v --arch pretty
 
-$PIDO --help
-$PIDO -Dkq --asdeps p1 --asexplicit p2 t1 t2
-$PIDO -Qcdegiklmnpqtu -o dog -s oops t1 
-$PIDO -Scgilquwyd -s oops t1 t2
-$PIDO --sync --nodeps --assume-installed p=v2 --dbonly --noprogressbar --noscriptlet --print --print-format "%d" t1
-$PIDO --sync --asdeps --asexplicit --ignore p1 --ignoregroup p2 --needed --overwrite glob* t1 t2
-$PIDO -Sps oops
-$PIDO -Sps
-$PIDO - t1 t2 t3
+echo "Testing sync options"
+# sync options
+$PIDO -Sccggiilquuwyy -s a -s b
+# sync standard options
+$PIDO -S -b /pacman -v --arch linux --color when --config config-file --debug \
+      --gpgdir gpgdir --logfile sync.log --disable-download-timeout --sysroot /sysroot \
+      --cachedir /a --cachedir /b --hookdir /c --hookdir /d --noconfirm --confirm
+$PIDO -S --noconfirm
+$PIDO -S --confirm
+$PIDO -S --noconfirm --confirm
+$PIDO -S --confirm --noconfirm
+# sync transaction options
+$PIDO -Sddp --assume-installed oops=1.0 --dbonly --noprogressbar --noscriptlet --print-format "%l"
+# upgrade options
+$PIDO -S --asdeps --asexplicit --ignore p1 --ignoregroup p2 --needed --overwrite glob*
+$PIDO -S --ignore p1,p2,p3
+$PIDO -S --ignoregroup g1,g2,g3
+$PIDO -S --overwrite g1*,g2*,g3*
+
+#$PIDO --help
+#$PIDO -Dkq --asdeps p1 --asexplicit p2 t1 t2
+#$PIDO -Qcdegiklmnpqtu -o dog -s oops t1 
+#$PIDO -Scgilquwyd -s oops t1 t2
+#$PIDO --sync --nodeps --assume-installed p=v2 --dbonly --noprogressbar --noscriptlet --print --print-format "%d" t1
+#$PIDO -Sps oops
+#$PIDO -Sps
+#$PIDO - t1 t2 t3
 exit
 
 
